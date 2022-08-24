b1fdbd851369c8371d50913b92a2d17a7d954bfc
pongtsu <alwayshungrytolearn@gmail.com>
2022-08-24 14:08:07 +0700

    feat: Get nft data from activeItem table.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,35 @@ import { useMoralisQuery } from "react-moralis"
 import styles from "../styles/Home.module.css"
 
 const Home: NextPage = () => {
-  // const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
-  //   "ActiveItem",
-  //   (query) => query.limit(10).descending("tokenId")
-  // )
+  const { data: listedNfts, isFetching: isfetchingListedNfts } =
+    useMoralisQuery("ActiveItem", (query) =>
+      query.limit(10).descending("tokenId")
+    )
 
-  // console.log(listedNfts)
+  console.log(listedNfts)
 
-  return <div className={styles.container}>Hi</div>
+  return (
+    <div className={styles.container}>
+      {isfetchingListedNfts ? (
+        <div>Loading Nft...</div>
+      ) : (
+        <div>
+          {listedNfts.map((nft) => {
+            const { marketplaceAddress, nftAddress, price, tokenId, seller } =
+              nft.attributes
+            return (
+              <div className="p-4">
+                <p className="text-xl">{`MarketPlace address: ${marketplaceAddress}`}</p>
+                <p className="text-xl">{`nft Address: ${nftAddress}`}</p>
+                <p className="text-xl">{`price: ${price}`}</p>
+                <p className="text-xl">{`tokenId: ${tokenId}`}</p>
+              </div>
+            )
+          })}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Home
